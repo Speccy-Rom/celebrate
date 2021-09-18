c6f9076570f204082b02b788cda28d62ebec93fe
Speccy-Rom <speccy.rom@gmail.com>
2021-09-18 17:27:52 +0300

    added: describe the package with the application for distutils/setuptools

---
+++ setup.py
@@ -0,0 +1,58 @@
+import os
+from importlib.machinery import SourceFileLoader
+
+from pkg_resources import parse_requirements
+from setuptools import find_packages, setup
+
+module_name = 'analyzer_service'
+
+module = SourceFileLoader(
+    module_name, os.path.join(module_name, '__init__.py')
+).load_module()
+
+
+def load_requirements(fname: str) -> list:
+    """Функция load_requirements нужна чтобы setup.py смог прочитать
+     зависимости из файлов requirements.txt и requirements.dev.txt:"""
+    requirements = []
+    with open(fname, 'r') as fp:
+        for req in parse_requirements(fp.read()):
+            extras = '[{}]'.format(','.join(req.extras)) if req.extras else ''
+            requirements.append(
+                '{}{}{}'.format(req.name, extras, req.specifier)
+            )
+    return requirements
+
+
+setup(
+    name=module_name,
+    version=module.__version__,
+    author=module.__author__,
+    author_email=module.__email__,
+    license=module.__license__,
+    description=module.__doc__,
+    long_description=open('README.md').read(),
+    url='https://github.com/Speccy-Rom/celebrate.git',
+    platforms='all',
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Natural Language :: Russian',
+        'Operating System :: MacOS',
+        'Operating System :: POSIX',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: Implementation :: CPython'
+    ],
+    python_requires='>=3.8',
+    packages=find_packages(exclude=['tests']),
+    install_requires=load_requirements('requirements.txt'),
+    extras_require={'dev': load_requirements('requirements.dev.txt')},
+    entry_points={
+        'console_scripts': [
+            '{0}-api = {0}.api.__main__:main'.format(module_name),
+            '{0}-db = {0}.db.__main__:main'.format(module_name)
+        ]
+    },
+    include_package_data=True
+)
