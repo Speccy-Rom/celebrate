b20af3a043c4e16d99ffae1d37247d4b8ab2bcbc
Speccy-Rom <speccy.rom@gmail.com>
2021-09-19 12:40:18 +0300

    describe: the database schema with SQLAlchemy objects

--- analyzer_service/db/schema.py
+++ analyzer_service/db/schema.py
@@ -1,4 +1,9 @@
-from sqlalchemy import MetaData
+from enum import Enum, unique
+
+from sqlalchemy import (
+    MetaData, Table, Column, Integer, ForeignKey, String, Date,
+    Enum as PgEnum, ForeignKeyConstraint
+)
 
 convention = {
     'all_column_names': lambda constraint, table: '_'.join([
@@ -16,3 +21,46 @@ convention = {
     'pk': 'pk__%(table_name)s'
 }
 metadata = MetaData(naming_convention=convention)
+
+
+@unique
+class Gender(Enum):
+    female = 'female'
+    male = 'male'
+
+
+imports_table = Table(
+    'imports',
+    metadata,
+    Column('import_id', Integer, primary_key=True)
+)
+
+citizens_table = Table(
+    'citizens',
+    metadata,
+    Column('import_id', Integer, ForeignKey('imports.import_id'), primary_key=True),
+    Column('citizens_id', Integer, primary_key=True),
+    Column('town', String, nullable=False, index=True),
+    Column('street', String, nullable=False),
+    Column('building', String, nullable=False),
+    Column('apartment', Integer, nullable=False),
+    Column('name', String, nullable=False),
+    Column('birth_date', Date, nullable=False),
+    Column('gender', PgEnum(Gender, name='gender'), nullable=False),
+)
+
+relations_table = Table(
+    'relations',
+    metadata,
+    Column('import_id', Integer, primary_key=True),
+    Column('citizen_id', Integer, primary_key=True),
+    Column('relative_id', Integer, primary_key=True),
+    ForeignKeyConstraint(
+        ('import_id', 'citizen_id'),
+        ('citizens.import_id', 'citizens.citizen_id')
+    ),
+    ForeignKeyConstraint(
+        ('import_id', 'relative_id'),
+        ('citizens.import_id', 'citizens.citizen_id')
+    ),
+)
